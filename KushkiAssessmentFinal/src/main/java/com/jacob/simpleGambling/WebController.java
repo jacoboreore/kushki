1d45b04cab05bf7f7389d6cdea5208c6b92dd135
jacob <jacob@BOSS>
2021-07-18 22:42:23 -0500

    [jacob] Just to be sure about this assessment

--- KushkiAssessmentFinal/src/main/java/com/jacob/simpleGambling/WebController.java
+++ KushkiAssessmentFinal/src/main/java/com/jacob/simpleGambling/WebController.java
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 import com.jacob.server.BizLogic;
+import com.jacob.simpleGambling.Gambler;
 
 /*
 * Class controladora de la pagina existente. 
@@ -35,33 +35,30 @@ public class WebController implements WebMvcConfigurer {
 		registry.addViewController("/results").setViewName("results");
 	}
 
-	
 	/*
-	* Metodo para post comun. 
-	*
-	* @author FUTURE KUSHKI ENGINEER
-	*         Jacob Orellana<br>
-	*         jacobore@hotmail.com<br>
-	* 
-	* @date 18/07/2021
-	* @version 1.0
-	*/
-	
+	 * Metodo para post comun.
+	 *
+	 * @author FUTURE KUSHKI ENGINEER Jacob Orellana<br> jacobore@hotmail.com<br>
+	 * 
+	 * @date 18/07/2021
+	 * 
+	 * @version 1.0
+	 */
+
 	@GetMapping("/")
 	public String showForm(Gambler gambler) {
 		return "form";
 	}
-	
+
 	/*
-	* Metodo para ejecucipon de biz logic. 
-	*
-	* @author FUTURE KUSHKI ENGINEER
-	*         Jacob Orellana<br>
-	*         jacobore@hotmail.com<br>
-	* 
-	* @date 18/07/2021
-	* @version 1.0
-	*/
+	 * Metodo para ejecucipon de biz logic.
+	 *
+	 * @author FUTURE KUSHKI ENGINEER Jacob Orellana<br> jacobore@hotmail.com<br>
+	 * 
+	 * @date 18/07/2021
+	 * 
+	 * @version 1.0
+	 */
 
 	@RequestMapping(value = "/bizLogic", method = RequestMethod.POST)
 	public String quitarCredito(@Valid Gambler gambler, @RequestParam(value = "action", required = false) String action,
@@ -72,17 +69,15 @@ public class WebController implements WebMvcConfigurer {
 		return "form";
 	}
 
-	
 	/*
-	* Metodo para ejecucipon de una espera para el app. 
-	*
-	* @author FUTURE KUSHKI ENGINEER
-	*         Jacob Orellana<br>
-	*         jacobore@hotmail.com<br>
-	* 
-	* @date 18/07/2021
-	* @version 1.0
-	*/
+	 * Metodo para ejecucipon de una espera para el app.
+	 *
+	 * @author FUTURE KUSHKI ENGINEER Jacob Orellana<br> jacobore@hotmail.com<br>
+	 * 
+	 * @date 18/07/2021
+	 * 
+	 * @version 1.0
+	 */
 
 	@RequestMapping(value = "/mensajeEspera", method = RequestMethod.GET)
 	@ResponseBody
@@ -94,18 +89,16 @@ public class WebController implements WebMvcConfigurer {
 		return "";
 	}
 
-	
 	/*
-	* Metodo extraer el graficodel bloque 1. 
-	*
-	* @author FUTURE KUSHKI ENGINEER
-	*         Jacob Orellana<br>
-	*         jacobore@hotmail.com<br>
-	* 
-	* @date 18/07/2021
-	* @version 1.0
-	*/
-	
+	 * Metodo extraer el graficodel bloque 1.
+	 *
+	 * @author FUTURE KUSHKI ENGINEER Jacob Orellana<br> jacobore@hotmail.com<br>
+	 * 
+	 * @date 18/07/2021
+	 * 
+	 * @version 1.0
+	 */
+
 	@RequestMapping(value = "/uno", method = RequestMethod.GET)
 	@ResponseBody
 	public String uno(@Valid Gambler gambler, @RequestParam(value = "action", required = false) String action,
@@ -123,16 +116,15 @@ public class WebController implements WebMvcConfigurer {
 	}
 
 	/*
-	* Metodo extraer el grafico del bloque 2. 
-	*
-	* @author FUTURE KUSHKI ENGINEER
-	*         Jacob Orellana<br>
-	*         jacobore@hotmail.com<br>
-	* 
-	* @date 18/07/2021
-	* @version 1.0
-	*/
-	
+	 * Metodo extraer el grafico del bloque 2.
+	 *
+	 * @author FUTURE KUSHKI ENGINEER Jacob Orellana<br> jacobore@hotmail.com<br>
+	 * 
+	 * @date 18/07/2021
+	 * 
+	 * @version 1.0
+	 */
+
 	@RequestMapping(value = "/dos", method = RequestMethod.GET)
 	@ResponseBody
 	public String dos(@Valid Gambler gambler, @RequestParam(value = "action", required = false) String action,
@@ -150,16 +142,15 @@ public class WebController implements WebMvcConfigurer {
 	}
 
 	/*
-	* Metodo extraer el grafico del bloque 3. 
-	*
-	* @author FUTURE KUSHKI ENGINEER
-	*         Jacob Orellana<br>
-	*         jacobore@hotmail.com<br>
-	* 
-	* @date 18/07/2021
-	* @version 1.0
-	*/
-	
+	 * Metodo extraer el grafico del bloque 3.
+	 *
+	 * @author FUTURE KUSHKI ENGINEER Jacob Orellana<br> jacobore@hotmail.com<br>
+	 * 
+	 * @date 18/07/2021
+	 * 
+	 * @version 1.0
+	 */
+
 	@RequestMapping(value = "/tres", method = RequestMethod.GET)
 	@ResponseBody
 	public String tres(@Valid Gambler gambler, @RequestParam(value = "action", required = false) String action,
@@ -174,6 +165,25 @@ public class WebController implements WebMvcConfigurer {
 		System.out.println("tres " + "/img/img" + gambler.getBlock3() + ".jpg");
 
 		return "/img/img" + gambler.getBlock3() + ".jpg";
+	}
+
+	/*
+	 * Metodo para ejecucipon de biz logic.
+	 *
+	 * @author FUTURE KUSHKI ENGINEER Jacob Orellana<br> jacobore@hotmail.com<br>
+	 * 
+	 * @date 18/07/2021
+	 * 
+	 * @version 1.0
+	 */
+
+	@RequestMapping(value = "/cashOut", method = RequestMethod.POST)
+	public String cashOut(@Valid Gambler gambler, @RequestParam(value = "action", required = false) String action,
+			BindingResult bindingResult) {
+
+		System.out.println("CREDITOS: " + gambler.getCreditos());
+
+		return "results";
 	}
 
 }
